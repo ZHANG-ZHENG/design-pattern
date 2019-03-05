fb4d7a15b34d7b562ed911bf061826cf735d5cc5
zhengzhang <35785077@qq.com>
2019-03-06 00:10:13 +0800

    future

---
+++ future/src/main/java/futureorigin/RealData.java
@@ -0,0 +1,20 @@
+package futureorigin;
+
+public class RealData implements Data {
+    protected String data;
+ 
+    public RealData(String data) {
+        //利用sleep方法来表示RealData构造过程是非常缓慢的
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        this.data = data + " dealed by RealData";
+    }
+
+    @Override
+    public String getResult() {
+        return data;
+    }
+}
